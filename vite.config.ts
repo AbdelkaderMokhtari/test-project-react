f1eca06078b6639982788d26977370fbb450fc67
Abdelkader Mokhtari <lm20lk.abdelkader.mokhtari@gmail.com>
2025-07-22 20:41:11 +0100

    Update vite.config.ts
    Signed-off-by: Abdelkader Mokhtari <lm20lk.abdelkader.mokhtari@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/test-project-react/', // ✅ هذا هو المهم
+  base: mode === 'development' ? '/' : '/test-project-react/',
+
   server: {
     host: "::",
     port: 8080,
